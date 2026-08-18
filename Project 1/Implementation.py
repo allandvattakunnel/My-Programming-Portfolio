618240c57990574f3ac9f2bc9eb709ee46e1e4b3
Alland Vattakunnel <a3219584@adelaide.edu.au>
2026-08-19 01:37:27 +0930

    Created the intial template for the client class, following the plan I created in Planning.py, I added key attributes, and use self to be able to assign those attributes to specific objects. However need to fix up the display method, as a Typeerror is appearing.

---
+++ Project 1/Implementation.py
@@ -0,0 +1,22 @@
+#Creating the Client Class
+
+class Client:
+    #constructing the client class
+    def __init__(self,client_id,name,email,phone_number):
+        self.client_id = client_id
+        self.name = name
+        self.email = email
+        self.phone_number = phone_number
+
+    #creating a method to allow clients to update their email address
+    def update_email (self,new_email):
+        print(f"Your email has updated to: {new_email}")
+
+    #display the client's key info
+    def display_client_info (self,client_id,name,email,phone_number):
+        print(f"Client ID:{client_id}")
+        print(f"Client Name: {name}")
+        print(f"Client Email{email}")
+        print(f"Client Phone Number{phone_number}")
+
+allan = Client ("1","Allan","allan@gmail.com",042333)
