3453e2dd2b15d5343998a3c3c0d9bf0a35eaa903
Alland Vattakunnel <a3219584@adelaide.edu.au>
2026-09-03 02:14:05 +0930

    Completion of workshop 1.2 + inclusion of first code review.

--- Project 1/Implementation.py
+++ Project 1/Implementation.py
@@ -21,39 +21,71 @@ class Client:
         print(f"Client Phone Number: {self.phone_number}")
 
 
-client_1 = Client("1", "Allan", "allan@gmail.com", "042333")
-client_1.display_client_info()
-client_1.update_email("bananas@gmail.com")
-client_1.display_client_info()
-
-# Creating the Account class.
-# account_id
-# account_type
-# opening_balance
-# additional attribute - closing_balance
-
-
 class Account:
     # Constructing the actual account class.
-    def __init__(self, account_id, account_type, current_balance, closing_balance):
+    def __init__(self, account_id, account_type, current_balance=0, is_active=True):
         self.account_id = account_id
         self.account_type = account_type
         self.current_balance = current_balance
-        self.closing_balance = closing_balance
+        self.is_active = is_active
 
     def deposit(self, deposit_amount):
-        print(f"Your initial balance is:")
-        self.current_balance = self.current_balance + deposit_amount
-        print(f"Your new current balance is: {self.current_balance}")
+        print(f"Your initial balance is: ${self.current_balance}")
+        self.current_balance += deposit_amount
+        print(f"Your new balance is: ${self.current_balance}")
 
     def withdraw(self, withdraw_amount):
         if withdraw_amount > self.current_balance:
             print(
-                f"Insufficient Funds in {self.account_id}: your balance is: {self.current_balance}")
+                f"Insufficient Funds in {self.account_id}: your balance is: $ {self.current_balance}")
         else:
-            print(f"{self.account_id}: withdrew {withdraw_amount}.")
-            print(f"The new balance is {self.current_balance}")
+            self.current_balance -= withdraw_amount
+            print(f"Account {self.account_id}: withdrew ${withdraw_amount}.")
+            print(f"The new balance is $ {self.current_balance}")
+
+    def deactivate_account(self):
+        if not self.is_active:
+            print(f"Account:{self.account_id} has already been deactivated.")
+            return
+        else:
+            self.is_active = False
+            print(f"Account: {self.account_id} has now been deactivated")
 
 
-account_1 = Account("1", "Savings", "100", "200")
-account_1.deposit("200")
+# Creating Client objects.
+client_1 = Client("1", "Allan", "allan@gmail.com", "0423339567")
+client_2 = Client("2", "Jamie", "jamie23@outlook.com", "0466763903")
+client_3 = Client("3", "Johnny", "johnny45@hotmail.com", "0434567340")
+
+# Creating Account objects.
+account_1 = Account("1", "Savings", 22050)
+account_2 = Account("2", "Everyday", 30056)
+account_3 = Account("3", "Savings", 100000)
+
+# Printing selected attributes from each object.
+# print(client_1.name)
+# print(client_2.email)
+# print(client_3.phone_number)
+# print(account_1.account_id)
+# print(account_2.account_type)
+# print(account_3.current_balance)
+
+# # Calling the Display and Update email methods for two client objects.
+# client_1.display_client_info()
+# client_1.update_email("allan1234@gmail.com")
+# client_1.display_client_info()
+
+# client_2.display_client_info()
+# client_2.update_email("jamie3564@gmail.com")
+# client_1.display_client_info()
+
+# Adding funds for two accounts
+account_1.deposit(200)
+print(account_1.current_balance)
+account_2.deposit(5000)
+print(account_2.current_balance)
+
+# Removing Funds from accounts
+account_1.withdraw(100)
+print(account_1.current_balance)
+account_2.withdraw(1000000000)
